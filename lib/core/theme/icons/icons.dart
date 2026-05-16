b0db3a351aba859f40db1dfc784d78b4ab171928
Egorezz <2771716@gmail.com>
2026-05-17 02:05:35 +0300

    feat: added login_screen_layout

---
+++ lib/core/theme/icons/icons.dart
@@ -0,0 +1,10 @@
+class AppIcons {
+  static String backArrow = 'assets/icons/back_arrow_icon.svg';
+  static String eye = 'assets/icons/eye_icon.svg';
+  static String eyeClosed = 'assets/icons/eyeClosed_icon.svg';
+  static String film = 'assets/icons/film_icon.svg';
+  static String goArrow = 'assets/icons/go_arrow_icon.svg';
+  static String letter = 'assets/icons/letter_icon.svg';
+  static String lock = 'assets/icons/lock_icon.svg';
+  static String person = 'assets/icons/person_icon.svg';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'core/themes/colors/colors.dart';
+import 'core/theme/colors/colors.dart';
+import 'core/theme/icons/icons.dart';
+import 'core/theme/fonts/fonts.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MovieQuestApp());
@@ -11,50 +14,73 @@ class MovieQuestApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: RegistrationScreen(),
+      theme: ThemeData(fontFamily: AppFonts.mainFont),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class RegistrationScreen extends StatefulWidget {
-  const RegistrationScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<RegistrationScreen> createState() => _RegistrationScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _RegistrationScreenState extends State<RegistrationScreen> {
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/background.png'),
-                fit: BoxFit.cover,
-              ),
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background.png',
+              fit: BoxFit.cover,
             ),
           ),
-          Positioned.fill(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 160,
-              ),
-              child: Column(
-                children: [
-                  Expanded(child: RegistrationContainer()),
-                  SizedBox(height: 39),
-                  Text(
-                    'Popa zalupa account? Pogna sosat',
-                    style: TextStyle(color: AppColors.textDefault),
-                  ),
-                ],
+          SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 40,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    LoginContainer(),
+                    const SizedBox(height: 39),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account?",
+                          style: TextStyle(
+                            color: AppColors.textDefault,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        GestureDetector(
+                          onTap: () {
+                            //TODO: make route to registration_screen
+                            print('someshit');
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: AppColors.main,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -64,14 +90,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   }
 }
 
-class RegistrationContainer extends StatefulWidget {
-  const RegistrationContainer({super.key});
+class LoginContainer extends StatefulWidget {
+  const LoginContainer({super.key});
 
   @override
-  State<RegistrationContainer> createState() => _RegistrationContainerState();
+  State<LoginContainer> createState() => _LoginContainerState();
 }
 
-class _RegistrationContainerState extends State<RegistrationContainer> {
+class _LoginContainerState extends State<LoginContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -84,15 +110,225 @@ class _RegistrationContainerState extends State<RegistrationContainer> {
       ),
       child: Column(
         children: [
+          SvgPicture.asset(AppIcons.film, height: 32),
+          SizedBox(height: 7),
           Text(
-            'Movie Quest',
+            'MovieQuest',
             style: TextStyle(
               color: AppColors.text,
               fontSize: 30,
               fontWeight: FontWeight.bold,
             ),
           ),
+          SizedBox(height: 7),
+          Text(
+            'Unlock the cinematic universe',
+            style: TextStyle(color: AppColors.text, fontSize: 16),
+          ),
+          SizedBox(height: 49),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Email Adres',
+              style: TextStyle(
+                color: AppColors.textSecond,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.4,
+              ),
+            ),
+          ),
+          SizedBox(height: 7),
+          AppTextField(hint: '@example.com', icon: AppIcons.letter),
+          SizedBox(height: 24),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Password',
+                style: TextStyle(
+                  color: AppColors.textSecond,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.4,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  //TODO: make route to registration_screen
+                  print('someshit');
+                },
+                child: Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                    color: AppColors.text,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: 1.4,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 7),
+          AppTextField(hint: 'password', icon: AppIcons.lock, isPassword: true),
+          SizedBox(height: 40),
+          AppGlowButton(
+            text: 'Sign In',
+            radius: 10,
+            height: 63,
+            width: double.infinity,
+            isIconStart: false,
+            fontSize: 24,
+            icon: AppIcons.goArrow,
+            onPressed: () {
+              //TODO: make authorization
+              print('signin');
+            },
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class AppTextField extends StatefulWidget {
+  const AppTextField({
+    super.key,
+    required this.hint,
+    required this.icon,
+    this.isPassword = false,
+    this.controller,
+  });
+
+  final String hint;
+  final String icon;
+  final bool isPassword;
+  final TextEditingController? controller;
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.isPassword;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: _isObscured,
+      cursorColor: AppColors.textSecond,
+      style: TextStyle(color: AppColors.textSecond),
+      decoration: InputDecoration(
+        hintText: widget.hint,
+        prefixIcon: Padding(
+          padding: EdgeInsets.all(14),
+          child: SvgPicture.asset(widget.icon),
+        ),
+
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isObscured = !_isObscured;
+                  });
+                },
+                icon: Padding(
+                  padding: EdgeInsets.all(4),
+                  child: SvgPicture.asset(
+                    _isObscured ? AppIcons.eyeClosed : AppIcons.eye,
+                  ),
+                ),
+              )
+            : null,
+
+        fillColor: AppColors.background,
+        filled: true,
+
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.hint),
+          borderRadius: BorderRadius.circular(8),
+        ),
+
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.hint),
+          borderRadius: BorderRadius.circular(8),
+        ),
+
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: AppColors.error),
+        ),
+      ),
+    );
+  }
+}
+
+class AppGlowButton extends StatelessWidget {
+  const AppGlowButton({
+    super.key,
+    required this.text,
+    required this.radius,
+    required this.height,
+    required this.width,
+    required this.isIconStart,
+    required this.fontSize,
+    this.onPressed,
+    required this.icon,
+  });
+
+  final String text;
+  final String icon;
+  final bool isIconStart;
+  final double radius;
+  final double height;
+  final double width;
+  final double fontSize;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: AppColors.main,
+        borderRadius: BorderRadius.circular(radius),
+        boxShadow: [
+          BoxShadow(color: AppColors.main, blurRadius: 10, spreadRadius: 2),
+        ],
+      ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.transparent,
+          shadowColor: AppColors.transparent,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(radius),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (isIconStart) SvgPicture.asset(icon),
+            SizedBox(width: 10),
+            Text(
+              text,
+              style: TextStyle(
+                color: AppColors.hint,
+                fontSize: fontSize,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(width: 10),
+            if (!isIconStart) SvgPicture.asset(icon),
+          ],
+        ),
       ),
     );
   }
